d58608731f281fa12dd7dc3635328a47fd9d64b4
Tarun Kumar <kumar1998.tarun@gmail.com>
2020-08-29 19:23:44 +0530

    Correct indentation.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 def preprocess_data(abstracts_path, titles_path):
 
-	print('Reading data ...')
+  print('Reading data ...')
   total_df = read_data(abstracts_path, titles_path)
 
   train_df, val_df = split_data(total_df)
@@ -14,52 +14,52 @@ def preprocess_data(abstracts_path, titles_path):
   return train_df, val_df, word2idx, idx2word
 
 def main():
-	choice = sys.argv[1]
-	load_path = None
-	try:
-		load_path = sys.argv[2]
-	except:
-		if choice in ['val', 'test'] and load_path is None:
-			print('Please specify some path to load model weights from.')
-			return
+  choice = sys.argv[1]
+  load_path = None
+  try:
+    load_path = sys.argv[2]
+  except:
+    if choice in ['val', 'test'] and load_path is None:
+      print('Please specify some path to load model weights from.')
+      return
 
-	test_file = None
-	try:
-		test_file = sys.argv[3]
-		if choice in ['train', 'val']:
-			print('This command is not supported.')
-			return
-	except:
-		if choice == 'test':
-			print('Please provide path to test csv file containing abstracts only.')
-			return
+  test_file = None
+  try:
+    test_file = sys.argv[3]
+    if choice in ['train', 'val']:
+      print('This command is not supported.')
+      return
+  except:
+    if choice == 'test':
+      print('Please provide path to test csv file containing abstracts only.')
+      return
 
-	print('Choice: ', choice)
-	if load_path is not None:
-		print('Load path: ', load_path)
-	if test_file is not None:
-		print('Test file: ', test_file)
+  print('Choice: ', choice)
+  if load_path is not None:
+    print('Load path: ', load_path)
+  if test_file is not None:
+    print('Test file: ', test_file)
 
-	train_df, val_df, word2idx, idx2word = preprocess_data('./data/abstracts.pkl', './data/titles.pkl')
-	print('Preparing embedding matrix ...')
-	emb_matrix = get_embedding_matrix(word2idx, idx2word, './data/glove_vectors.txt', 'glove')
-	summarization = Summarization(emb_matrix, emb_dim=300, hidden_dim=128, word2idx=word2idx, idx2word=idx2word)
-	print('Preparing Input data ...')
-	if choice == 'train':
-		train_data = prepare_input_data(train_df, word2idx)
-		summarization.train(train_data, use_prev=load_path)
-	elif choice == 'val':
-		eval_data = prepare_input_data(val_df, word2idx)
-		summarization.eval(eval_data, val_df, load_path=load_path, evaluation='val', print_samples=True)
-	elif choice == 'test':
-		test_df, eval_data = prepare_test_data(test_file, word2idx)
-		summarization.eval(eval_data, test_df, load_path=load_path, evaluation='test', print_samples=True)
-	else:
-		print('This command is not supported.')
-		return
+  train_df, val_df, word2idx, idx2word = preprocess_data('./data/abstracts.pkl', './data/titles.pkl')
+  print('Preparing embedding matrix ...')
+  emb_matrix = get_embedding_matrix(word2idx, idx2word, './data/glove_vectors.txt', 'glove')
+  summarization = Summarization(emb_matrix, emb_dim=300, hidden_dim=128, word2idx=word2idx, idx2word=idx2word)
+  print('Preparing Input data ...')
+  if choice == 'train':
+    train_data = prepare_input_data(train_df, word2idx)
+    summarization.train(train_data, use_prev=load_path)
+  elif choice == 'val':
+    eval_data = prepare_input_data(val_df, word2idx)
+    summarization.eval(eval_data, val_df, load_path=load_path, evaluation='val', print_samples=True)
+  elif choice == 'test':
+    test_df, eval_data = prepare_test_data(test_file, word2idx)
+    summarization.eval(eval_data, test_df, load_path=load_path, evaluation='test', print_samples=True)
+  else:
+    print('This command is not supported.')
+    return
 
 if __name__ == '__main__':
-	main()
+  main()
 
 
 
